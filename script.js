620a5d11e8f8ca70862da7f4aeb23e6925dd71ef
Uncp1 <113035612+Uncp1@users.noreply.github.com>
2022-11-21 15:16:08 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
 const newButton = document.querySelector('.profile__add-button');
 const edditButton = document.querySelector('.profile__edit-button');
 const addButton = document.querySelector('#add-button');
@@ -33,81 +6,83 @@ const popupProfile = document.querySelector('#popup-profile');
 const popupAdd = document.querySelector('#popup-add');
 const saveButton = document.querySelector('.form__button');
 const elementsContainer = document.querySelector('.elements')
+const popupImg = document.querySelector('.popup_img');
+const elementTemplate = document.querySelector('#element-template').content;
+const profileName = document.querySelector('.profile__name');
+const profileOccupation= document.querySelector('.profile__occupation');
+const name = document.querySelector('#img-name');
+const link = document.querySelector('#img-link');
 
-let inputs = document.querySelectorAll('input');
-let profileName = document.querySelector('.profile__name');
-let profileOccupation= document.querySelector('.profile__occupation');
-inputs[0].value = profileName.textContent;
-inputs[1].value = profileOccupation.textContent;  // нужно связать с плейсхолдером -\_/-
-
-function handleFormSubmit(evt) {
-  evt.preventDefault();
-  profileName.textContent = inputs[0].value;
-  profileOccupation.textContent = inputs[1].value;;
-  popupWindow[0].classList.remove('popup_opened');
+function openPopup(popup){
+  popup.classList.add('popup_opened');
 }
 
-function addElement(name, link) {
-  const elementTemplate = document.querySelector('#element-template').content;
+function closePopup(popup){
+  popup.classList.remove('popup_opened');
+}
+
+function addCard(name, link) {
   const element = elementTemplate.querySelector('.element').cloneNode(true);
-  const popupImg = element.querySelector('.popup_img');
   const deleteButton = element.querySelector('.element__delete');
+  const cardImg = element.querySelector('.element__pic');
+  element.querySelector('.element__name').textContent = name;
+  cardImg.setAttribute('alt', name);
+  cardImg.setAttribute('src', link);
 
   element.querySelector('.element__like').addEventListener('click', function (evt) {
     evt.target.classList.toggle('element__like_active');
   }); 
 
-  element.querySelector('.element__pic').addEventListener('click', function() {
-    popupImg.classList.add('popup_opened');
-  }); 
-
   deleteButton.addEventListener('click', function(){
-    console.log('buba');
     const binItem = deleteButton.closest('.element');
-    console.log(binItem);
     binItem.remove();
-    inItem.parentNode.removeChild(binItem);
   })
 
-  element.querySelector('.element__name').textContent = name;
-  element.querySelector('.element__pic').setAttribute('alt', name);
-  element.querySelector('.element__pic').setAttribute('src', link);
-  element.querySelector('.element__modal-name').textContent = name;
-  element.querySelector('.element__modal-pic').setAttribute('src', link);
-  element.querySelector('.element__modal-pic').setAttribute('alt', name);
-  elementsContainer.append(element);
+  element.querySelector('.element__pic').addEventListener('click', function() {
+    openPopup(popupImg);
+    cardPopupImg = popupImg.querySelector('.element__modal-pic');
+    popupImg.querySelector('.element__modal-name').textContent = name;
+    cardPopupImg.setAttribute('src', link);
+    cardPopupImg.setAttribute('src', link);
+  }); 
 
-  let exitButton = document.querySelectorAll('.popup__exit-button');
-  exitButton.forEach((btn)=>{
-    btn.addEventListener('click', function (evt) {
-      evt.target.closest('.popup').classList.remove('popup_opened');
-    }); 
-  });
+  elementsContainer.insertBefore(element, elementsContainer.firstChild);
 }
 
 initialCards.forEach((item) => {
-  addElement(item.name, item.link);
+  addCard(item.name, item.link);
 }); 
 
+const buttonsClosePopup = document.querySelectorAll('.popup__exit-button');
+  buttonsClosePopup.forEach((btn)=>{
+    btn.addEventListener('click', function (evt) {
+      closePopup(evt.target.closest('.popup'));
+    }); 
+  });
+
 newButton.addEventListener('click', function(){
-  popupAdd.classList.add('popup_opened');
+  openPopup(popupAdd);
 });
 
-edditButton.addEventListener('click', function edditProfile(){
-  popupProfile.classList.add('popup_opened');
+edditButton.addEventListener('click', function(){
+  document.querySelector('#form-name').value= profileName.textContent;
+  document.querySelector('#form-occupation').value= profileOccupation.textContent;
+  openPopup(popupProfile);
 });
 
 saveButton.addEventListener('click', handleFormSubmit);
 
 addButton.addEventListener('click', function (evt) {
   evt.preventDefault();
-  const name = document.querySelector('#img-name');
-  const link = document.querySelector('#img-link');
-
-  addElement(name.value, link.value);
-  
+  addCard(name.value, link.value);
   name.value = '';
   link.value = '';
-
-  evt.target.closest('.popup').classList.remove('popup_opened');
+  closePopup(evt.target.closest('.popup'));
 });
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = document.querySelector('#form-name').value;
+  profileOccupation.textContent = document.querySelector('#form-occupation').value;
+  closePopup(evt.target.closest('.popup'));
+}
